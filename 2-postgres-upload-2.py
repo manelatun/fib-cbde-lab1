70eae74c14ffb47d4080c0d888bc0c0166eb08d3
Eric Luque Luque <eric.luquex2@gmail.com>
2026-09-26 21:33:57 +0200

    Separar inserción frases y embeddings 2-postgres-upload-2.py

--- 2-postgres-upload-2.py
+++ 2-postgres-upload-2.py
@@ -10,13 +10,21 @@ from config import postgres_config
 # https://www.psycopg.org/docs/usage.html
 conn = psycopg2.connect(postgres_config)
 cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS sentences CASCADE")
+cur.execute("""
+DROP TABLE IF EXISTS sentences CASCADE;
+DROP TABLE IF EXISTS embeddings CASCADE;
+""")
 cur.execute("""
 CREATE TABLE sentences (
   id INTEGER PRIMARY KEY,
-  sentence TEXT,
-  embedding DOUBLE PRECISION[]
+  sentence TEXT
 )
+""")
+cur.execute("""
+CREATE TABLE embeddings (
+  sentence_id INTEGER PRIMARY KEY references sentences(id),
+  embedding DOUBLE PRECISION[]
+  )
 """)
 
 print("Cargando embeddings.json")
@@ -30,30 +38,57 @@ load_time = end - start
 
 print(f"{result['count']} embeddings, generados originalmente en {result['elapsed']} (Cargados en {load_time})")
 print()
-print("Subiendo a PostgreSQL")
+print("Subiendo frases a PostgreSQL")
 
-insert_times = []
+sentences_insert_times = []
 
-# inserta los embedddings de cada frase del dataset en PostgreSQL
+# inserta las frases del dataset en PostgreSQL
 for row in result['rows']:
   start = time.perf_counter()
-  cur.execute("INSERT INTO sentences (id, sentence, embedding) VALUES (%s, %s, %s)", (row['id'], row['sentence'], row['embedding']))
+  cur.execute("INSERT INTO sentences (id, sentence) VALUES (%s, %s)", (row['id'], row['sentence']))
   end = time.perf_counter()
-  insert_times.append(end - start)
+  sentences_insert_times.append(end - start)
 
 start = time.perf_counter()
 conn.commit()
 end = time.perf_counter()
-commit_time = end - start
+sentences_commit_time = end - start
+
+
+print("Subiendo embeddings a PostgreSQL")
+
+embeddings_insert_times = []
+
+# inserta los embeddings del dataset en PostgreSQL
+for row in result['rows']:
+  start = time.perf_counter()
+  cur.execute("INSERT INTO embeddings (sentence_id, embedding) VALUES (%s, %s)", (row['id'], row['embedding']))
+  end = time.perf_counter()
+  embeddings_insert_times.append(end - start)
+
+start = time.perf_counter()
+conn.commit()
+end = time.perf_counter()
+embeddings_commit_time = end - start
 
 cur.close()
 conn.close()
 
 # output de los tiempos de inserción
-print("Mean:", statistics.mean(insert_times))
-print("Median:", statistics.median(insert_times))
-print("Standard deviation:", statistics.stdev(insert_times))
-print("Min:", min(insert_times))
-print("Max:", max(insert_times))
-print("Total:", sum(insert_times))
-print("+Commit:", commit_time)
+print("Tiempo de insercion de las frases: ")
+print("Mean:", statistics.mean(sentences_insert_times))
+print("Median:", statistics.median(sentences_insert_times))
+print("Standard deviation:", statistics.stdev(sentences_insert_times))
+print("Min:", min(sentences_insert_times))
+print("Max:", max(sentences_insert_times))
+print("Total:", sum(sentences_insert_times))
+print("+Commit:", sentences_commit_time)
+
+print("Tiempo de insercion de los embeddings: ")
+print("Mean:", statistics.mean(embeddings_insert_times))
+print("Median:", statistics.median(embeddings_insert_times))
+print("Standard deviation:", statistics.stdev(embeddings_insert_times))
+print("Min:", min(embeddings_insert_times))
+print("Max:", max(embeddings_insert_times))
+print("Total:", sum(embeddings_insert_times))
+print("+Commit:", embeddings_commit_time)
